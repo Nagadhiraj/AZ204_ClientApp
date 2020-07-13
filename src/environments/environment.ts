bfcb0bbd33130bc9762e6da1b130abcbc1eb4174
Nagadhiraj <dharchaudhuri23@gmail.com>
2020-07-13 08:54:20 +0530

    Update to environment.ts commit 5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: false,
   //userUrl: "https://hfstappahqpfgasla6am-userfuncapp.azurewebsites.net/api/",
-  userUrl: "https://localhost:44382/",
+  userUrl: "https://hfapp-webapp-eu.azurewebsites.net/",
   clientId: 'ef4c5495-06f8-4af1-9801-9a24bafc9584',
   authority: 'https://login.microsoftonline.com/3b9fe888-a28f-4637-8247-e7d730502d46/',
-  redirectUri: 'http://localhost:4200/',
-  postLogoutRedirectUri: 'https://hfstapp-cdn.azureedge.net',
+  redirectUri: 'https://hfapp-cdnendpoint.azureedge.net/',
+  postLogoutRedirectUri: 'https://hfapp-cdnendpoint.azureedge.net/',
   isIE: window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1,
   consentScopes1: ["api://4500db5b-cf58-4b2f-98b2-d5ce8f0f4fc6/api-access"],
   unprotectedResources: [],
-  webApi1: 'https://hfstappahqpfgasla6am-userfuncapp.azurewebsites.net/api/',
+  webApi1: 'https://hfapp-webapp-eu.azurewebsites.net/',
   extraQueryParameters: {}
 };
 
